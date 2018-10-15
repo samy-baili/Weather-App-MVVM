e36796b2b55e7f772fafd21fa08e15edf533c5fb
samy-baili <samy.baili@gmail.com>
2018-10-15 11:28:50 +0400

    Fix Cleartext HTTP traffic not permitted issue

--- app/src/main/java/android/apps/hbmsu/com/weatherapp/service/api/NetworkManager.java
+++ app/src/main/java/android/apps/hbmsu/com/weatherapp/service/api/NetworkManager.java
@@ -11,6 +11,7 @@ import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Single;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -20,10 +21,15 @@ public class NetworkManager {
     private static NetworkService networkService = configureService();
 
     private static NetworkService configureService() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .connectTimeout(1, TimeUnit.MINUTES)
                 .readTimeout(1, TimeUnit.MINUTES)
                 .writeTimeout(1, TimeUnit.MINUTES)
+                .addInterceptor(interceptor)
                 .build();
 
         return new Retrofit.Builder()
